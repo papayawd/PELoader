1423b7b97e573881940d8205f47b89b67e2d8169
papayawd <269086687@qq.com>
2021-11-17 15:34:26 +0800

    同步测试

--- main.py
+++ main.py
@@ -3,21 +3,22 @@ from binascii import *
 
 
 class PE:
-    def GetValue(self, offset, type='dword'):
-        if(type == 'word'):
+    def getvalue(self, offset, type='dword'):
+        if type == 'word':
             return hexlify(self.FileBuff[offset:offset + 2])
         if type == 'dword':
             return hexlify(self.FileBuff[offset:offset + 4])
 
     def __init__(self, FileBuff):
         self.FileBuff = FileBuff
-        self.e_magic = self.GetValue(offset=0,type='word')
+        self.e_magic = self.getvalue(offset=0, type='word')
 
     def info(self):
         print(type(self.e_magic))
         print('MZ标志位 {}'.format(self.e_magic))
 
-with open('./Lab03-03.exe','rb+') as f:
+
+with open('./Lab03-03.exe', 'rb+') as f:
     FileBuffer = f.read()
-    #print(FileBuffer)
+    # print(FileBuffer)
     PE(FileBuffer).info()
