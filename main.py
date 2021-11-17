ca9ae4a13b90488721cab09b493609e575126c48
papayawd <269086687@qq.com>
2021-11-17 23:44:16 +0800

    添加节区    完结撒花

--- main.py
+++ main.py
@@ -78,6 +78,39 @@ class IMAGE_DATA_DIRECTORY:
         self.Size = self.getvalue(offset=self.offset + 0x4)
 
 
+class IMAGE_SECTION_HEADER:
+
+    def getvalue(self, offset, size='dword'):
+        if size == 'byte':
+            return int(hexlify(self.FileBuff[offset:offset + 1][::-1]), 16)
+
+        if size == 'word':
+            return int(hexlify(self.FileBuff[offset:offset + 2][::-1]), 16)
+
+        if size == 'dword':
+            return int(hexlify(self.FileBuff[offset:offset + 4:][::-1]), 16)
+
+    def getstring(self, offset):
+        s = ''
+        cnt = 0
+        while self.getvalue(offset=offset, size='byte') != 0:
+            s += chr(self.getvalue(offset=offset, size='byte'))
+            offset += 1
+            if ++cnt == 8:  # 对section的name特殊判断一下 因为只有8字节供存储
+                break
+        return s
+
+    def __init__(self, FileBuff, offset):
+        self.FileBuff = FileBuff
+        self.offset = offset
+        self.Name = self.getstring(offset=self.offset)
+        self.VirtualSize = hex(self.getvalue(offset=self.offset + 0x8))
+        self.VirtualAddress = hex(self.getvalue(offset=self.offset + 0xc))
+        self.SizeOfRawData = hex(self.getvalue(offset=self.offset + 0x10))
+        self.PointerToRawData = hex(self.getvalue(offset=self.offset + 0x14))
+        self.Characteristics = hex(self.getvalue(offset=self.offset + 0x24))
+
+
 class PE:
 
     def getvalue(self, offset, size='dword'):
@@ -102,7 +135,7 @@ class PE:
         self.FileHeader = self.NT_header + 0x4
         self.NumberOfSections = self.getvalue(offset=self.FileHeader + 0x2, size='word')
         self.TimeDateStamp = hex(self.getvalue(offset=self.FileHeader + 0x4))
-        self.SizeOfOptionalHeader = hex(self.getvalue(offset=self.FileHeader + 0x10))
+        self.SizeOfOptionalHeader = self.getvalue(offset=self.FileHeader + 0x10, size='word')
 
         # 可选PE头
         self.OP_Header = self.NT_header + 0x18
@@ -115,7 +148,6 @@ class PE:
         self.SectionAlignment = hex(self.getvalue(offset=self.OP_Header + 0x20))  # 内存对齐
         self.FileAlignment = hex(self.getvalue(offset=self.OP_Header + 0x24))  # 文件对齐
         self.SizeOfImage = hex(self.getvalue(offset=self.OP_Header + 0x38))
-        self.SizeOfHeaders = hex(self.getvalue(offset=self.OP_Header + 0x3c))  # PE头大小
         self.CheckSum = hex(self.getvalue(offset=self.OP_Header + 0x40))  # 校验和
 
         # 导出表
@@ -128,6 +160,8 @@ class PE:
         self.IMAGE_DIRECTORY_ENTRY_IAT = IMAGE_DATA_DIRECTORY(FileBuff=self.FileBuff,
                                                               offset=self.OP_Header + 0x60 + 12 * 0x8)
 
+        self.SectionBegin = self.NT_header + 0x18 + self.SizeOfOptionalHeader # DOS头+PE头+可选PE头
+
     def info(self):
         print(type(self.e_magic))
         print('*****************DOS头*********************')
@@ -135,13 +169,13 @@ class PE:
         print('\n')
 
         print('*****************PE头*********************')
-        print('==> PE文件头大小: {}'.format(self.SizeOfHeaders))
+        print('==> PE文件头大小: {}'.format(hex(0x14)))
         print('PE标志位: {}'.format(self.PE_Signature))
         print('时间戳: {}'.format(self.TimeDateStamp))
         print('\n')
 
         print('*****************可选PE头*********************')
-        print('==> 可选PE头大小: {}'.format(self.SizeOfOptionalHeader))
+        print('==> 可选PE头大小: {}'.format(hex(self.SizeOfOptionalHeader)))
         print('魔数: {}'.format(self.Magic))
         print('程序入口点: {}'.format(self.AddressOfEntryPoint))
         print('代码段基地址: {}'.format(self.BaseOfCode))
@@ -175,13 +209,22 @@ class PE:
             while IMAGE_THUNK_DATA32(self.FileBuff, THUNK_OFFSET).Ordinal != 0:
                 thunk_name = IMAGE_THUNK_DATA32(self.FileBuff, THUNK_OFFSET)
                 thunk_func = IMAGE_THUNK_DATA32(self.FileBuff, FUNC_OFFSET)
-                print('\t{}\t\t\t\t{}'.format(thunk_name.Name, thunk_func.function))
+                print('\t{}\t\t\t\t{}'.format(thunk_name.Name, thunk_func.function))  ## 这里的对齐有点点丑  有空再来想办法改一改
                 FUNC_OFFSET += 0x4
                 THUNK_OFFSET += 0x4
             print('\n')
-
+        print(hex(self.SectionBegin))
         print('*****************节区*********************')
+        print('区段名称\t\t文件内大小\t\t文件内偏移\t\t对齐后大小\t\t对齐后偏移\t\t属性')
+        SECTION_OFFSET = self.SectionBegin
 
+        for i in range(self.NumberOfSections):
+            section = IMAGE_SECTION_HEADER(self.FileBuff, SECTION_OFFSET)
+            print('{}\t\t{}\t\t\t{}\t\t\t{}\t\t\t{}\t\t\t{}'.format(section.Name, section.VirtualSize, section.VirtualAddress,
+                                                            section.SizeOfRawData, section.PointerToRawData,
+                                                            section.Characteristics))
+
+            SECTION_OFFSET += 0x28
 
 
 if __name__ == '__main__':
